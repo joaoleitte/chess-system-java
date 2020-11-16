8e1982f470ff22538591d35580e3a4117e1f26bb
João Bezerra <joao.bezerra30@hotmail.com>
2020-11-16 15:08:58 -0300

    ChessException and ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,7 +7,7 @@ public class Program {
 	public static void main(String[] args) {
 
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoard(chessMatch.getPieces());		
+		UI.printBoard(chessMatch.getPieces());	
 	}
 
 }
